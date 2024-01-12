a24f2e9ad8627d17b5b373d2e03b1ab7285b66e9
Astivero-x3d <ismaelast2005@gmail.com>
2024-01-12 12:10:31 +0100

    Haciendo la practica

---
+++ Inventario/src/main/java/arrays/inventario/Inventario.java
@@ -0,0 +1,17 @@
+/*
+ * Practica de inventario
+ */
+
+package arrays.inventario;
+
+/**
+ *
+ * @author Ismael_Ast
+ */
+public class Inventario {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Solucion");
+    }
+}
